7d1e3cd33205031171da8c48d35d69c42d55ccc6
FunkyWonder <nathan.wassink@gmail.com>
2022-10-13 01:22:51 +0200

    Changes to be committed: 	new file:   .import/Logo.png-b94b928db9902e61fdadadb7b4484507.md5 	new file:   .import/Logo.png-b94b928db9902e61fdadadb7b4484507.stex 	new file:   .import/Sprite sheet.png-b43b1fb943118e33c8307041a9e668cb.md5 	new file:   .import/Sprite sheet.png-b43b1fb943118e33c8307041a9e668cb.stex 	new file:   .import/logo_placeholder.png-e0ec13aa4c127b7e0341b90e773dbee7.md5 	new file:   .import/logo_placeholder.png-e0ec13aa4c127b7e0341b90e773dbee7.stex 	new file:   Assets/Animations/Idle.tres 	new file:   Assets/Animations/Run.tres 	new file:   Assets/Fonts/Montserrat-ExtraBold.ttf 	new file:   Assets/Fonts/montserrat.tres 	new file:   Assets/Image/Logo.png 	new file:   Assets/Image/Logo.png.import 	modified:   Assets/Image/Sprite sheet.piskel 	modified:   Assets/Image/Sprite sheet.png 	new file:   Assets/Image/Sprite sheet.png.import 	new file:   Assets/Image/logo_placeholder.png 	new file:   Assets/Image/logo_placeholder.png.import 	new file:   Scenes/FadeIn.tscn 	new file:   Scenes/LevelSelect.tscn 	renamed:    main.tscn -> Scenes/Levels/Level 1.tscn 	renamed:    Tilemap.tscn -> Scenes/Tilemap.tscn 	new file:   Scenes/TitleScreen.tscn 	modified:   project.godot 	new file:   src/GUI/FadeIn.gd 	new file:   src/GUI/LevelSelect.gd 	new file:   src/GUI/MenuButton.gd 	new file:   src/GUI/ReturnToTitle.gd 	new file:   src/GUI/TitleScreen.gd 	renamed:    src/Player.gd -> src/Player/Player_Movement.gd 	deleted:    src/test.gd

---
+++ src/GUI/LevelSelect.gd
@@ -0,0 +1,8 @@
+extends Button
+
+export(String) var scene_to_load
+
+func _on_Level_1_pressed():
+	connect("pressed", self, "_on_Button_pressed", [scene_to_load])
+	
+	get_tree().change_scene(scene_to_load)
--- src/Player/Player_Movement.gd
+++ src/Player/Player_Movement.gd
@@ -7,12 +7,12 @@ const maxMovementSpeed = 40
 const jumpForce = 250
 
 var motion = Vector2()
+var facingRight = true
 
 func _ready():
 	pass # Replace with function body.
 
-
-func _physics_process(delta):
+func _physics_process(_delta):
 	var input = false
 	motion.y += gravity
 	
@@ -23,18 +23,44 @@ func _physics_process(delta):
 	
 	if Input.is_action_pressed("right"):
 		input = true
+		facingRight = true
 		motion.x = motion.x + maxMovementSpeed
+		$AnimationPlayer.play("Run")
 	
 	if Input.is_action_pressed("left"):
 		input = true
+		facingRight = false
 		motion.x = motion.x - maxMovementSpeed
-		
+		$AnimationPlayer.play("Run")
+	
 	if input == false:
 		motion.x = lerp(motion.x, 0, 0.2)
+		$AnimationPlayer.play("Idle")
+		
+		if facingRight == true:
+			$Sprite.scale.x = -1
+		else:
+			$Sprite.scale.x = 1
+	else:
+		if facingRight == true:
+			$Sprite.scale.x = 1
+		else:
+			$Sprite.scale.x = -1
 	
 	if is_on_floor():
 		if Input.is_action_pressed("jump"):
 			motion.y = -jumpForce
 	
+	if !is_on_floor():
+		if motion.y < 0:
+			$AnimationPlayer.play("Jump")
+			
+			if facingRight == true:
+				$Sprite.scale.x = 1
+			else:
+				$Sprite.scale.x = -1
+			
+		elif motion.y > 0:
+			print("Falling")
+	
 	motion = move_and_slide(motion, up)
-
